5e266ec2b5fdb098aaef32d93384f43d21dd1c5b
BuildTools <unconfigured@null.spigotmc.org>
2017-04-03 12:45:30 -0400

    add serveIndex

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
-var express = require('express');
-var path = require('path');
+var express     = require('express');
+var path        = require('path');
+var serveIndex  = require('serve-index');
 
 var app = express();
 
+
 app.use(express.static(path.join(__dirname, 'public')));
+app.use('/js', serveIndex('/public/js', {icons: true}));
 
 var listener = app.listen(process.env.PORT || 3000, function() {
     console.log('App listening at http://localhost:' + listener.address().port);
