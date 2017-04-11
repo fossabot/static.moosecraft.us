defdd11691cda12d122344584821893f550fce64
moose51789 <moose51789@gmail.com>
2017-04-11 17:00:21 -0400

    basic 404 handling, dumped to see in the logs for debugging purposes.

--- index.js
+++ index.js
@@ -9,6 +9,12 @@ app.use(require('express-status-monitor')());
 // let express serve static files
 app.use(express.static(path.join(__dirname, 'public')));
 
+// catch 404 errors to output them into the console log
+app.use(function (req, res, next) {
+    console.log(req);
+    res.status(404).send('Unable to get the resource requested');
+})
+
 var listener = app.listen(process.env.PORT || 3000, function() {
     console.log('App listening at http://localhost:' + listener.address().port);
 });
