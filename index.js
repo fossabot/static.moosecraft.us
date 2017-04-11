7cb1e9b98fab72960c04b3e5f8f45e11a82b5edc
moose51789 <moose51789@gmail.com>
2017-04-11 18:00:26 -0400

    remove compression

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-var compression = require('compression');
 var express     = require('express');
 var path        = require('path');
 
@@ -7,14 +6,11 @@ var app = express();
 // add monitoring middleware
 app.use(require('express-status-monitor')());
 
-// add gzip compression to all assets
-//app.use(compression);
-
 // let express serve static files
 app.use(express.static(path.join(__dirname, 'public')));
 
 // catch 404 errors to output them into the console log
-app.use(function (req, res, next) {
+app.use(function (req, res) {
     console.log("Requested URL: " + req.url + " Status: 404");
     res.status(404).send('Unable to get the resource requested');
 })
