d9564d402bc11e70d5c990fef3ca45373fa1c75c
moose51789 <moose51789@gmail.com>
2017-04-11 15:28:47 -0400

    add spdy

--- index.js
+++ index.js
@@ -1,13 +1,22 @@
 var express     = require('express');
+var spdy        = require('spdy');
 var path        = require('path');
-var serveIndex  = require('serve-index');
+var fs          = require('fs');
 
 var app = express();
 
+const options = {
+    key: fs.readFileSync('/etc/letsencrypt/live/static.moosecraft.us/privkey.pem'),
+    cert: fs.readFileSync('/etc/letsencrypt/live/static.moosecraft.us/cert.pem')
+}
 
 app.use(express.static(path.join(__dirname, 'public')));
-app.use(serveIndex('/', {icons: true}));
 
-var listener = app.listen(process.env.PORT || 3000, function() {
-    console.log('App listening at http://localhost:' + listener.address().port);
+var listener = spdy.createServer(options, app).listen(process.env.PORT || 3000, (error) => {
+    if (error) {
+        console.error(error);
+        return process.exit(1);
+    } else {
+        console.log('App listening at http://localhost:' + listener.address().port);
+    }
 });
